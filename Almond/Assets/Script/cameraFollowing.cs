059040e4aff7ccabcf869b10291ac5663e9d1aab
Levi Tamath <tam19365@uvg.edu.gt>
2021-10-02 13:33:49 -0600

    wall

--- Almond/Assets/Script/cameraFollowing.cs
+++ Almond/Assets/Script/cameraFollowing.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera_Following : MonoBehaviour
+public class cameraFollowing : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset;
